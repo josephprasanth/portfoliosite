df6137fd2ce825e9f5e0917e1d36fff238682fed
josephprasanth <prasanthjoseph_77@yahoo.com>
2019-06-04 15:38:40 +0200

    Added initial test intro section

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello </h1>
+        <p>My name is Joseph. I'm a software engineer</p>
+        <p>I am always on the lookout for challenging projects</p>
+      </div>
+    );
+  }
+}
+
+export default App;
